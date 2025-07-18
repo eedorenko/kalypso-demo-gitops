75793dfd7aad6d6d7544e59a1a4d3dbdce2806ed
Kalypso Scheduler <kalypso.scheduler@email.com>
2025-07-18 19:47:40 +0000

    Kalypso Scheduler commit

--- vm/hello-int-test/platform-config.sh
+++ vm/hello-int-test/platform-config.sh
@@ -8,4 +8,12 @@ export ENVIRONMENT="Dev"
 export REGION="EU"
 
 export SOME_COMMON_ENVIRONMENT_VARIABLE="true"
+
+export azureTenantId="16b3c013-d300-468d-ac64-7eda0820b6d3"
+
+export keyVaultSecrets="[objectName: application-secret\nobjectType: secret\n]"
+
+export keyvaultName="ansible-secret-key-vault"
+
+export secretObjects="[map[data:[map[sourcePath:application-secret targetKey:application-secret-value]] secretName:application-secret type:Opaque]]"
            
