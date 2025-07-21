aa4ef9b3cac7e102964a9b3e12ecffc3e6088a16
Kalypso Scheduler <kalypso.scheduler@email.com>
2025-07-21 23:47:59 +0000

    Kalypso Scheduler commit

--- vm/hw-dev/platform-config.sh
+++ vm/hw-dev/platform-config.sh
@@ -15,5 +15,7 @@ export keyVaultSecrets="[objectName: application-secret\nobjectType: secret\n]"
 
 export keyvaultName="ansible-secret-key-vault"
 
+export managedIdentityId="332f57f9-6de8-4929-ad1e-f1f3015ee0ae"
+
 export secretObjects="[map[data:[map[sourcePath:application-secret targetKey:application-secret-value]] type:Opaque]]"
            
