d3619b3f79c8fb067f34cb2b265a8bce18f339a2
Kalypso Scheduler <kalypso.scheduler@email.com>
2025-07-22 22:54:23 +0000

    Kalypso Scheduler commit

--- vm/hw-dev/platform-config.sh
+++ vm/hw-dev/platform-config.sh
@@ -11,11 +11,11 @@ export SOME_COMMON_ENVIRONMENT_VARIABLE="true"
 
 export azureTenantId="16b3c013-d300-468d-ac64-7eda0820b6d3"
 
-export keyVaultSecrets="[objectName: application-secret\nobjectType: secret\n]"
+export keyVaultSecrets="[objectName: application-secret\nobjectType: secret\n objectName: acr-secret\nobjectType: secret\n objectName: git-hub-token\nobjectType: secret\n]"
 
 export keyvaultName="ansible-secret-key-vault"
 
 export managedIdentityId="332f57f9-6de8-4929-ad1e-f1f3015ee0ae"
 
-export secretObjects="[map[data:[map[sourcePath:application-secret targetKey:application-secret-value]] type:Opaque]]"
+export secretObjects="[map[data:[map[sourcePath:application-secret targetKey:application-secret-value]] name:application-secret type:Opaque] map[data:[map[sourcePath:git-hub-user targetKey:username] map[sourcePath:git-hub-token targetKey:password]] name:gh-repo-secret type:Opaque]]"
            
